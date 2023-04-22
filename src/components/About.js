d8cd9148e18dbf783f7d6bf4007e869710d9d3ae
GFerreiroS <seba.gabi@gmail.com>
2023-04-22 17:20:16 +0000

    All components are h-screen and Download CV is functional

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
+import CV from "../CV/CV.pdf"
 
 export default function About() {
     return (
-        <section id="about">
+        <section id="about" className="h-screen flex">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
@@ -26,9 +27,11 @@ export default function About() {
                     </p>
                     <div className="flex justify-center">
                         <a
-                            href="#contact"
+                            href={CV}
+                            download="Gabriel_Ferreiro_CV.pdf"
+                            target="_blank"
                             className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                            Work With Me
+                            Download CV
                         </a>
                         <a
                             href="#projects"
@@ -39,9 +42,9 @@ export default function About() {
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
-                        className="object-cover object-center rounded"
+                        className="object-cover object-center rounded-full"
                         alt="hero"
-                        src="./coding.svg"
+                        src="./Me.jpeg"
                     />
                 </div>
             </div>
